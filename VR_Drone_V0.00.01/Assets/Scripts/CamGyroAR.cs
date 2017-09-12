f44642ef984fae560a84a10552ae59bb26ea0072
Patrick Stockton <pstockton@ieee.org>
2017-09-11 18:37:32 -0700

    Uploaded project files.
    Added Unity project files to master branch.

---
+++ VR_Drone_V0.00.01/Assets/Scripts/CamGyroAR.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamGyroAR : MonoBehaviour {
+
+	public GameObject webPlane;
+	GameObject camParent;
+	
+	// Use this for initialization
+	void Start () {
+		
+		camParent = new GameObject("CamParent");
+		camParent.transform.position = this.transform.position;
+		this.transform.parent = camParent.transform;
+		camParent.transform.Rotate(Vector3.right, 90);
+		Input.gyro.enabled = true;
+		
+		WebCamTexture webcamTexture = new WebCamTexture();
+		webPlane.GetComponent<MeshRenderer>().material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion rotFix = new Quaternion(Input.gyro.attitude.x,
+											Input.gyro.attitude.y,
+											-Input.gyro.attitude.z,
+											-Input.gyro.attitude.w);
+		this.transform.localRotation = rotFix;
+	}
+}
